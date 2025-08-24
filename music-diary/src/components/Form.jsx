464f2e28d2ce8dabab111d72ba8fa2b1c76f9ede
Oconnor Burton <oconnorburton387@gmail.com>
2025-08-23 22:45:54 -0500

    starting frontend

---
+++ music-diary/src/components/Form.jsx
@@ -0,0 +1,58 @@
+const Info = (props) =>{
+    return(
+        <>
+        <div className="input">
+            {props.label}
+            <input type={props.type} />
+
+        </div>
+        
+        </>
+    )
+}
+
+const Select = (props) =>{
+    return(
+        <div className="input">
+            {props.label}
+            <select>
+                {
+                    props.options.map(op => (
+                        <option>{op}</option>
+                    ))
+                }
+            </select>
+        </div>
+        
+    )
+}
+
+const Form = () =>{
+    const genre = ["dancehall", "reggae", "afrobeats", "hip hop"]
+    return(
+        
+        <>
+        <div className="form">
+            <h2>Add a new song</h2>
+            <hr />
+            <form>
+                
+
+                <Info label = "Artist" type="text"/>
+                <Info label = "Artist" type="text"/>
+                <Select label="Genre" options={genre}/>
+                <Select label="Genre" options={genre}/>
+                <Info label = "Artist"/>
+                <Info label = "Artist"/>
+
+                <button type="submit" className="submitBtn">Add Song</button>
+
+            </form>
+            
+        </div>
+        
+        </>
+    )
+}
+
+export default Form
